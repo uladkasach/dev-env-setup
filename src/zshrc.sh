8b13a009cb82b7aabe467e5ae04a25aaa2bfb5cb
Casey Brookes <58667871+caseybrookes@users.noreply.github.com>
2026-03-03 10:27:28 -0600

    fix(install): make portable

--- src/zshrc.sh
+++ src/zshrc.sh
@@ -117,7 +117,7 @@ export AWS_SDK_LOAD_CONFIG=1
 [[ -f "$HOME/.cargo/env" ]] && source "$HOME/.cargo/env"
 
 # pnpm link --global
-export PNPM_HOME="/home/vlad/.local/share/pnpm"
+export PNPM_HOME="$HOME/.local/share/pnpm"
 case ":$PATH:" in
   *":$PNPM_HOME:"*) ;;
   *) export PATH="$PNPM_HOME:$PATH" ;;
